9c88e00f8d42f179fa17074f74ce603e480cb335
SofiMorter <65443565+SofiMorter@users.noreply.github.com>
2020-07-08 17:59:59 +0600

    palindrome

---
+++ task2.py
@@ -0,0 +1,20 @@
+# Написать функцию, которая определяет,
+# является ли целое число палиндромом.
+# Числовой палиндром читается слева направо
+# и справа налево одинаково, например 101. 
+
+# Пример работы функции:
+# is_palindrome(353) # True
+# is_palindrome(-111) # False
+# is_palindrome(42) # False
+
+def is_palindrome(num):
+    palindrome_input_list = [int(x) for x in str(num)]
+    palindrome_reverse = palindrome_input_list[ -1:: -1]
+    if palindrome_input_list == palindrome_reverse:
+        print('eto palindrom')
+    else:
+        print('eto hernia kakaya-to')
+
+
+is_palindrome(363)
